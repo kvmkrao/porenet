c9ed8c890496ea952e86536b56cb50308d0e80d6
V Kotteda <kvmkrao@gmail.com>
2021-08-27 11:58:59 -0600

    unsteady incompressible flow simulator

---
+++ incomp_flow_unsteady/run.sh
@@ -0,0 +1,7 @@
+
+python 3d_openpnm_simple_gen.py 
+cmake . 
+make 
+./sinphase  -ksp_type fgmres  -pc_type hypre -ksp_max_it=60000 -ksp_atol=1e-10
+#./sinphase  -ksp_type fgmres  -pc_type hypre  -mat_view -ksp_max_it=60000 -ksp_atol=1e-10
+#display mass.png 
